96e21db1d04778e548d336bda5f5d84ee593ecba
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-01-08 14:45:22 +0100

    Add comments

--- date.h
+++ date.h
@@ -1,6 +1,19 @@
 #ifndef DATE_H_
 #define DATE_H_
 
+/*
+ * Given a date specification for a cookie expiration date,
+ * compute the correct time_t.  Examples of date specs are:
+ *
+ *   "now"
+ *   a number => epoch
+ *   a string with "sign" "number" "type"
+ *     sign can be + or -
+ *     number is an offset
+ *     type is y, M, d, h, m, s
+ *     this will be added / subtracted to the current time
+ */
+
 #include "buffer.h"
 
 double date_compute(const char *date);
