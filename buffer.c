748eb018e033d7b672c53e28ead4dacd644db1cf
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-01-08 10:38:28 +0100

    Small buffers are now stored without memory allocation; yay!

--- buffer.c
+++ buffer.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "gmem.h"
@@ -7,75 +6,41 @@
 #define BUFFER_SIZE_INIT   32
 #define BUFFER_SIZE_FACTOR 2
 
+static void buffer_zero(Buffer* buffer)
+{
+    buffer->size = 0;
+    buffer->pos = 0;
+    buffer->data = 0;
+}
+
 Buffer* buffer_init(Buffer* buffer, unsigned int size)
 {
-    int target = size > 0 ? size : BUFFER_SIZE_INIT;
+    buffer_zero(buffer);
 
-    GMEM_NEW(buffer->data, char*, target);
-    buffer->size = target;
+    unsigned int target = size > 0 ? size+1 : BUFFER_SIZE_INIT;
+
+    if (size > sizeof(buffer->fixed)) {
+        GMEM_NEW(buffer->data, char*, target);
+        buffer->size = target;
+    } else {
+        buffer->data = buffer->fixed;
+        buffer->size = sizeof(buffer->fixed);
+    }
 
     return buffer_reset(buffer);
 }
 
 Buffer* buffer_fini(Buffer* buffer)
 {
-    if (buffer->data) {
+    if (buffer->data &&
+        buffer->data != buffer->fixed) {
         GMEM_DEL(buffer->data, char*, buffer->size);
     }
-    buffer->size = 0;
-    buffer->pos = 0;
-    buffer->data = 0;
+
+    buffer_zero(buffer);
 
     return buffer;
 }
-
-Buffer* buffer_wrap(Buffer* buffer, const char* data, int length)
-{
-    buffer->data = (char*) data;
-    buffer->pos = 0;
-    if (length < 0) {
-        length = strlen(data);
-    }
-    buffer->size = length;
-
-    return buffer;
-}
-
-Buffer* buffer_ensure_total(Buffer* buffer, int size)
-{
-    int needed = size + 1;
-    if (buffer->size >= needed) {
-        return buffer;
-    }
-
-    int target = BUFFER_SIZE_INIT;
-    while (target < needed) {
-        target *= BUFFER_SIZE_FACTOR;
-    }
-
-    GMEM_REALLOC(buffer->data, char*, buffer->size, target);
-    buffer->size = target;
-
-    return buffer;
-}
-
-Buffer* buffer_ensure_delta(Buffer* buffer, int size)
-{
-    int needed = size + 1;
-    int left = buffer->size - buffer->pos;
-    if (needed <= left) {
-        return buffer;
-    }
-
-    return buffer_ensure_total(buffer, buffer->pos + size);
-}
-
-#if 0
-int buffer_left(const Buffer* buffer)
-{
-    return buffer->size - buffer->pos;
-}
-#endif
 
 Buffer* buffer_reset(Buffer* buffer)
 {
@@ -92,14 +57,62 @@ Buffer* buffer_rewind(Buffer* buffer)
 
 Buffer* buffer_terminate(Buffer* buffer)
 {
-    if (buffer->size > buffer->pos) {
+    if (buffer->pos < buffer->size) {
         buffer->data[buffer->pos] = '\0';
     }
     return buffer;
 }
 
-Buffer* buffer_append(Buffer* buffer, const char* source, int length)
+Buffer* buffer_wrap(Buffer* buffer, const char* data, unsigned int length)
 {
+    buffer_zero(buffer);
+
+    if (length == 0 && data[0] != '\0') {
+        length = strlen(data) + 1;
+    }
+    buffer->size = length;
+    buffer->data = (char*) data;
+
+    return buffer;
+}
+
+Buffer* buffer_ensure_total(Buffer* buffer, unsigned int size)
+{
+    unsigned int needed = size + 1;
+    if (buffer->size >= needed) {
+        return buffer;
+    }
+
+    unsigned int target = BUFFER_SIZE_INIT;
+    while (target < needed) {
+        target *= BUFFER_SIZE_FACTOR;
+    }
+
+    if (buffer->data == buffer->fixed) {
+        GMEM_NEW(buffer->data, char*, target);
+        memcpy(buffer->data, buffer->fixed, buffer->size);
+    } else {
+        GMEM_REALLOC(buffer->data, char*, buffer->size, target);
+    }
+    buffer->size = target;
+
+    return buffer;
+}
+
+Buffer* buffer_ensure_delta(Buffer* buffer, unsigned int size)
+{
+    unsigned int needed = size + 1;
+    unsigned int left = buffer->size - buffer->pos;
+    if (needed <= left) {
+        return buffer;
+    }
+
+    return buffer_ensure_total(buffer, buffer->pos + size);
+}
+
+Buffer* buffer_append(Buffer* buffer, const char* source, unsigned int length)
+{
+    buffer_ensure_delta(buffer, length);
     memcpy(buffer->data + buffer->pos, source, length);
     buffer->pos += length;
     return buffer;
--- buffer.h
+++ buffer.h
@@ -1,24 +1,27 @@
 #ifndef BUFFER_H_
 #define BUFFER_H_
 
+#define BUFFER_SIZEOF_DESIRED 48
+
 typedef struct Buffer {
-    char* data;
-    int pos;
+    unsigned int pos;
     unsigned int size;
+    char* data;
+    char fixed[BUFFER_SIZEOF_DESIRED - 2*sizeof(unsigned int) - 1*sizeof(char*)];
 } Buffer;
 
 Buffer* buffer_init(Buffer* buffer, unsigned int size);
 Buffer* buffer_fini(Buffer* buffer);
 
-Buffer* buffer_wrap(Buffer* buffer, const char* data, int length);
+Buffer* buffer_wrap(Buffer* buffer, const char* data, unsigned int length);
 
-Buffer* buffer_ensure_total(Buffer* buffer, int size);
-Buffer* buffer_ensure_delta(Buffer* buffer, int size);
+Buffer* buffer_ensure_total(Buffer* buffer, unsigned int size);
+Buffer* buffer_ensure_delta(Buffer* buffer, unsigned int size);
 
 Buffer* buffer_reset(Buffer* buffer);
 Buffer* buffer_rewind(Buffer* buffer);
 Buffer* buffer_terminate(Buffer* buffer);
 
-Buffer* buffer_append(Buffer* buffer, const char* source, int length);
+Buffer* buffer_append(Buffer* buffer, const char* source, unsigned int length);
 
 #endif
