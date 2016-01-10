ff338588d590b0bd2ae985625ec6d7f88377719c
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-01-10 17:44:54 +0100

    Include Cookie::Baker in benchmark (commented out)

--- tools/bench.pl
+++ tools/bench.pl
@@ -3,10 +3,14 @@ use strict;
 use warnings;
 use blib;
 use Dumbbench;
+use Cookie::Baker ();
 use Cookie::Baker::XS ();
 use Devel::Cookie ();
 
-my $cookie = 'foo=bar; path=/';
+# my $cookie = 'foo=bar; path=/';
+#my $cookie = 'DV=; expires=Mon, 01-Jan-1990 00:00:00 GMT; path=/webhp; domain=www.google.com';
+my $cookie = 'whv=MtW_XszVxqHnN6rHsX0d; expires=Wed, 07 Jan 2026 11:10:40 GMT; domain=.wikihow.com; path=';
+my $iterations = 1e5;
 
 my $bench = Dumbbench->new(
     target_rel_precision => 0.005,
@@ -14,10 +18,19 @@ my $bench = Dumbbench->new(
 );
 
 $bench->add_instances(
+    # Dumbbench::Instance::PerlSub->new(
+    #     name => 'Cookie::Baker',
+    #     code => sub {
+    #         for(1..$iterations){
+    #             Cookie::Baker::crush_cookie($cookie);
+    #         }
+    #     },
+    # ),
+
     Dumbbench::Instance::PerlSub->new(
         name => 'Cookie::Baker::XS',
         code => sub {
-            for(1..1e5){
+            for(1..$iterations){
                 Cookie::Baker::XS::crush_cookie($cookie);
             }
         },
@@ -26,7 +39,7 @@ $bench->add_instances(
     Dumbbench::Instance::PerlSub->new(
         name => 'Devel::Cookie',
         code => sub {
-            for(1..1e5){
+            for(1..$iterations){
                 Devel::Cookie::crush_cookie($cookie);
             }
         },
@@ -35,4 +48,3 @@ $bench->add_instances(
 
 $bench->run;
 $bench->report;
-
