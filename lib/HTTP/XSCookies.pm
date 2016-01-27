ae8fe01ddbc813a5be3f642d21e99e520112ee3c
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-01-27 09:46:44 +0100

    Bump version

--- lib/HTTP/XSCookies.pm
+++ lib/HTTP/XSCookies.pm
@@ -6,7 +6,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.000003';
+our $VERSION = '0.000004';
 XSLoader::load( 'HTTP::XSCookies', $VERSION );
 
 our @EXPORT_OK = qw[bake_cookie crush_cookie];
@@ -25,7 +25,7 @@ HTTP::XSCookies - Quick & dirty cookie mangling for Perl
 
 =head1 VERSION
 
-Version 0.000003
+Version 0.000004
 
 =head1 SYNOPSIS
 
